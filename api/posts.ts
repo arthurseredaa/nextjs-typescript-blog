61b8f74c00af050217d0901c0fd95e16447a2129
arthurseredaa <neonbetcy@gmail.com>
2021-03-24 19:32:48 +0200

    Create fetch post by id func

--- api/posts.ts
+++ api/posts.ts
@@ -4,3 +4,8 @@ export const getPosts = async () => {
   const data = await axiosInstance.get("/").then(res => res.data)
   return data;
 }
+
+export const getPost = async (id) => {
+  const data = await axiosInstance.get(`/${id}`).then(res => res.data);
+  return data
+}

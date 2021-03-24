7bb26cbf2221db4b9167f908edf7c37bac68f43c
arthurseredaa <neonbetcy@gmail.com>
2021-03-24 18:17:48 +0200

    Create button component

---
+++ components/Button.tsx
@@ -0,0 +1,43 @@
+import { FC, ReactNode } from "react";
+import styled, { keyframes } from "styled-components"
+
+interface ButtonProps {
+  background?: string;
+  color?: string;
+  padding?: string;
+  children?: ReactNode
+}
+
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotateZ(0deg);
+  }
+  25% {
+    transform: rotateZ(5deg);
+  }
+  50% {
+    transform: rotateZ(-10deg);
+  }
+  75% {
+    transform: rotateZ(5deg);
+  }
+  100% {
+    transform: rotateZ(0deg);
+  }
+`;
+
+const StyledButton = styled.button`
+  background-color: ${({ background = "#000" }: ButtonProps) => background};
+  color: ${({ color = "#fff" }: ButtonProps) => color};
+  padding: ${({ padding = "5px 10px" }: ButtonProps) => padding};
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: 500;
+  &:hover {
+    animation: ${rotateAnimation} .3s linear;
+  }
+`;
+
+export const Button: FC<ButtonProps> = ({ children, ...props }: ButtonProps) => <StyledButton {...props}>{children}</StyledButton>;

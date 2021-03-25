468257e486f86a7ecaa170f32e9f37c17d81d2b5
arthurseredaa <neonbetcy@gmail.com>
2021-03-25 13:45:33 +0200

    Styles

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,10 +1,36 @@
+import styled from "styled-components";
 import { getPost, getPosts } from "../../api/posts";
+import { Flex } from "../../components/Flex";
 import { Layout } from "../../components/Layout";
 
-function PostItem({post}) {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+const StyledPostItem = styled.div`
+  & > h1 {
+    font-family: 'Oswald', sans-serif;
+    font-size: 70px;
+    background-color: black;
+    color: white;
+    display: inline-block;
+    line-height: 1;
+    text-transform: uppercase;
+  }
+`;
+
+function PostItem({ post }) {
+  const { title, body } = post;
   return (
     <Layout pageName={post.title}>
-      Post item {post.id}
+      <Flex width="50%" margin="50px auto 0" direction="column">
+        <StyledPostItem>
+          <h1>{title}</h1>
+          <h3>{body}</h3>
+        </StyledPostItem>
+      </Flex>
     </Layout>
   )
 }
@@ -12,7 +38,7 @@ function PostItem({post}) {
 export async function getStaticPaths() {
   const posts = await getPosts();
 
-  const paths = posts.map((post: { id: number, title: string, body: string }) => ({ params: { id: post.id.toString() } }));
+  const paths = posts.map((post: Post) => ({ params: { id: post.id.toString() } }));
   return { paths, fallback: false }
 }
 

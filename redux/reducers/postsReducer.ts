45b2109ade0ccdff0f5a9cc44444c2df0e901def
arthurseredaa <neonbetcy@gmail.com>
2021-03-25 18:43:17 +0200

    Reducer of all posts

--- redux/reducers/postsReducer.ts
+++ redux/reducers/postsReducer.ts
@@ -1,11 +1,17 @@
+import { PostsAction, PostsActionTypes } from '../ts-types';
+
 const initialState = {
   posts: []
 }
 
-export const postsReducer = (state = initialState, action) => {
+export const postsReducer = (state = initialState, action: PostsAction) => {
   switch(action.type) {
-    case "Text":
-      return {...state, test: "text"}
+    case PostsActionTypes.SET_POSTS:
+      return {...state, posts: [...action.payload]};
+    case PostsActionTypes.SET_NEW_POST:
+      return {...state, posts: [...state.posts, action.payload]}
+    case PostsActionTypes.DELETE_POST:
+      return {...state, posts: state.posts.filter(({id}) => id !== action.payload)}
     default:
       return state;
   }

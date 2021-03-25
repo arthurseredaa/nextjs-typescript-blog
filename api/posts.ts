c7ed058df5515a3225b188875583cd59658c60ba
arthurseredaa <neonbetcy@gmail.com>
2021-03-25 13:45:10 +0200

    Change create post func

--- api/posts.ts
+++ api/posts.ts
@@ -5,7 +5,24 @@ export const getPosts = async () => {
   return data;
 }
 
-export const getPost = async (id) => {
+export const getPost = async (id: string) => {
   const data = await axiosInstance.get(`/${id}`).then(res => res.data);
   return data
+}
+
+// export const createPost = async (data: {title: string, body: string}) => {
+//   const res = await axiosInstance.post("/", {
+//     body: data,
+//     headers: {'Content-Type': 'application/json'}
+//   }).then(res => res.data);
+//   return res;
+// }
+
+export const createPost = async (data: {title: string, body: string}) => {
+  const res = await fetch("https://simple-blog-api.crew.red/posts ", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  }).then(res => res.json());
+  return res;
 }

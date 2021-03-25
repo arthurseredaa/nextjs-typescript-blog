3c22af9bbe023ff1320352bb9a30ace259b19e58
arthurseredaa <neonbetcy@gmail.com>
2021-03-25 18:43:24 +0200

    Main reducer

--- redux/reducers/rootReducer.ts
+++ redux/reducers/rootReducer.ts
@@ -1,6 +1,10 @@
+import { currentPostReducer } from './currentPostReducer';
 import { postsReducer } from './postsReducer';
 import { combineReducers } from "redux";
 
 export const rootReducer = combineReducers({
-  posts: postsReducer
+  posts: postsReducer,
+  currentPost: currentPostReducer
 })
+
+export type RootState = ReturnType<typeof rootReducer>

cefc96138dc194965d2b6afcdb0fba575ee1542e
arthurseredaa <neonbetcy@gmail.com>
2021-03-25 12:23:38 +0200

    Add new styles to post card

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { FC } from "react"
+import styled from "styled-components"
 
 interface PostProps {
   id: number;
@@ -7,11 +8,32 @@ interface PostProps {
   body: string;
 }
 
+const StyledCard = styled.div`
+  width: 30%;
+  border: 1px solid gray;
+  margin: 40px 20px;
+  padding: 10px 20px;
+  transition: .1s all linear;
+  border-radius: 3px;
+  cursor: pointer;
+  & > h1 {
+    font-family: 'Oswald', sans-serif;
+    text-transform: uppercase;
+  };
+  & > p {
+    font-family: 'Roboto', sans-serif;
+    font-size: 17px;
+  };
+  &:hover {
+    box-shadow: 5px 5px 10px black
+  }
+`;
+
 export const PostCard: FC<PostProps> = ({ id, title, body }) => (
   <Link href={`/posts/${id}`} passHref>
-    <div>
+    <StyledCard>
       <h1>{title}</h1>
       <p>{body}</p>
-    </div>
+    </StyledCard>
   </Link>
 )

b1ab551e1b41f91d7b12acdf3a6a2eb66db913b9
arthurseredaa <neonbetcy@gmail.com>
2021-03-25 12:23:45 +0200

    Styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home({ posts }: HomeProps) {
 
   return (
     <Layout pageName="Home">
-      <Flex margin="0 auto" width="50vw">
+      <Flex margin="50px auto 0" width="1200px" wrap="wrap" justify="flex-start">
         {
           posts && posts.map((post) => <PostCard {...post} />)
         }

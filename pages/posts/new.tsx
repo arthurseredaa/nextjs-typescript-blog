fd0b111f5b6f3b7186ee7099846e4bdbf804ef7c
arthurseredaa <neonbetcy@gmail.com>
2021-03-25 19:55:36 +0200

    Add update functionallity

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@material-ui/core";
 import { useRouter } from "next/dist/client/router";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
-import { createPost } from "../../api/posts";
+import { createPost, getPost, updatePost } from "../../api/posts";
 import { Flex } from "../../components/Flex";
 import { Layout } from "../../components/Layout";
-import { setNewPost } from "../../redux/actions/posts";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+import { setIsUpdate, setNewPost } from "../../redux/actions/posts";
 
 //  36
 
@@ -46,6 +47,22 @@ function CreatePost() {
     body: "",
   });
 
+  const currentPost = useTypedSelector(state => state.currentPost.currentPost);
+  const isUpdate = useTypedSelector(state => state.currentPost.isUpdate);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      if (isUpdate) {
+        let res = await getPost(currentPost);
+        const { title, body } = res;
+        setState({ title, body });
+      }
+    }
+
+    fetchPost();
+  }, [isUpdate])
+
+
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement> | ChangeEvent<HTMLInputElement>) => setState({ ...state, [e.target.name]: e.target.value });
 
   const handleSubmit = async (e) => {
@@ -61,14 +78,21 @@ function CreatePost() {
     }
   }
 
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const res = await updatePost(state, currentPost);
+    dispatch(setIsUpdate(false));
+    router.push(`/posts/${res.id}`)
+  }
+
   return (
     <Layout pageName="Create post">
       <Flex width="70%" margin="60px auto 0" justify="center" align="center">
-        <StyledForm onSubmit={handleSubmit}>
+        <StyledForm onSubmit={(e) => isUpdate ? handleUpdate(e) : handleSubmit(e)}>
           <Flex direction="column">
             <StyledTitle placeholder="Title" name="title" onChange={handleChange} value={state.title} />
             <StyledBody placeholder="Body" name="body" onChange={handleChange} value={state.body} />
-            <Button type="submit" variant="contained">Create</Button>
+            <Button type="submit" variant="contained">{isUpdate ? "Update" : "Create"}</Button>
           </Flex>
         </StyledForm>
       </Flex>

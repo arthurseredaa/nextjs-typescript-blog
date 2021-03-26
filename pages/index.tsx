8fb907830ac7ff2903e53a8005c588d1c16b2afd
arthurseredaa <neonbetcy@gmail.com>
2021-03-26 03:08:21 +0200

    Check if has posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,11 +22,13 @@ export default function Home({ data }: HomeProps) {
     <Layout pageName="Home">
       <Flex margin="50px auto 0" width="1200px" wrap="wrap" justify="flex-start">
         {
-          posts && posts.length > 0 && posts.map((post) => {
+          posts && posts.length > 0 ? posts.map((post) => {
             if(post.title && post.body) {
               return <PostCard key={post.id} {...post} />
             }
-          })
+          }) : (
+            <h1 style={{margin: "200px auto 0", opacity: ".3"}}>Cannot find any post :(</h1>
+          )
         }
       </Flex>
     </Layout>
